e5616f8b464c4e0e5a77e607761dfc2b463d2c02
s330mph <s330mph@gmail.com>
2025-06-17 01:30:26 +0300

    add TestBoxTests

---
+++ src/test/java/tests/TestBoxTests.java
@@ -0,0 +1,28 @@
+package tests;
+
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestBoxTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.holdBrowserOpen = true;
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+
+    @Test
+    void fillFormTest() {
+        open("/automation-practice-form");
+        $("#firstName").setValue("Ivan");
+    }
+}
