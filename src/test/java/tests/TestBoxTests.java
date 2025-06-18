48134981aec8d54e00dda5573672595c2c1466b2
s330mph <s330mph@gmail.com>
2025-06-18 22:20:51 +0300

    add full test fillFormTet

--- src/test/java/tests/TestBoxTests.java
+++ src/test/java/tests/TestBoxTests.java
@@ -5,6 +5,8 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
@@ -27,11 +29,25 @@ public class TestBoxTests {
         $("#firstName").setValue("Ivan");
         $("#lastName").setValue("Ivanov");
         $("#userEmail").setValue("ivan@ivanov.com");
-        $("#genterWrapper > div.col-md-9.col-sm-12 > div:nth-child(3) > label").click();
-        $("#userNumber").setValue("+79999999999");
+        $("[for=gender-radio-3]").click();
+        $("#userNumber").setValue("8999999999");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__day.react-datepicker__day--007.react-datepicker__day--weekend").click();
-        $("#hobbiesWrapper > div.col-md-9.col-sm-12 > div:nth-child(3) > label").click();
+        $("[for=hobbies-checkbox-3]").click();
+        File fileToUpload = new File("D:/QA/Artefacts/160х160.png");
+        $("[id=uploadPicture]").uploadFile(fileToUpload);
+        $("#currentAddress").setValue("1123321123");
+        $("#stateCity-wrapper").$(byText("Select State")).click();
+        $("#stateCity-wrapper").$(byText("Uttar Pradesh")).click();
+        $("#stateCity-wrapper").$(byText("Select City")).click();
+        $("#stateCity-wrapper").$(byText("Lucknow")).click();
+        $("#submit").click();
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+
+
+
+
+
 
 
     }
