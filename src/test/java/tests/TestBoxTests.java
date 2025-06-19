d27012a56521f759550998fb7c800c5d5883ec73
s330mph <s330mph@gmail.com>
2025-06-19 21:26:40 +0300

    added search male, gender by text added resourses directory added checking in submitted table

--- src/test/java/tests/TestBoxTests.java
+++ src/test/java/tests/TestBoxTests.java
@@ -5,8 +5,6 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
@@ -25,29 +23,42 @@ public class TestBoxTests {
     @Test
     void fillFormTest() {
         open("/automation-practice-form");
-        $("#firstName").setValue("Ivan");
-        $("#lastName").setValue("Ivanov");
-        $("#userEmail").setValue("ivan@ivanov.com");
-        $("[for=gender-radio-3]").click();
-        $("#userNumber").setValue("8999999999");
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__day.react-datepicker__day--007.react-datepicker__day--weekend").click();
-        $("[for=hobbies-checkbox-3]").click();
-        File fileToUpload = new File("D:/QA/Artefacts/160х160.png");
-        $("[id=uploadPicture]").uploadFile(fileToUpload);
-        $("#currentAddress").setValue("1123321123");
-        $("#stateCity-wrapper").$(byText("Select State")).click();
+        $("#firstName").setValue("Ivan");  //firstname
+        $("#lastName").setValue("Ivanov");  //lastname
+        $("#userEmail").setValue("ivan@ivanov.com");  // email
+        $("#genterWrapper").$(byText("Male")).click(); // gender
+        $("#userNumber").setValue("8999999999");  // number
+        $("#hobbiesWrapper").$(byText("Music")).click();  //hobbies
+
+        $("#dateOfBirthInput").clear();  //date
+        $(".react-datepicker__month-select").selectOption("April");
+        $(".react-datepicker__year-select").selectOption("1990");
+        $(".react-datepicker__day--010").click();
+
+        $("#subjectsInput").setValue("Math").pressEnter();  // subject
+
+        $("#uploadPicture").uploadFromClasspath("160х160.png");  //picture
+
+        $("#stateCity-wrapper").$(byText("Select State")).click();  //city
         $("#stateCity-wrapper").$(byText("Uttar Pradesh")).click();
         $("#stateCity-wrapper").$(byText("Select City")).click();
         $("#stateCity-wrapper").$(byText("Lucknow")).click();
+
+        //checking
         $("#submit").click();
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
 
-
-
-
-
-
+        $(".table-responsive").shouldHave(text("Ivan"));
+        $(".table-responsive").shouldHave(text("Ivanov"));
+        $(".table-responsive").shouldHave(text("ivan@ivanov.com"));
+        $(".table-responsive").shouldHave(text("Male"));
+        $(".table-responsive").shouldHave(text("8999999999"));
+        $(".table-responsive").shouldHave(text("Music"));
+        $(".table-responsive").shouldHave(text("160х160.png"));
+        $(".table-responsive").shouldHave(text("10 April,1990"));
+        $(".table-responsive").shouldHave(text("Math"));
+        $(".table-responsive").shouldHave(text("Uttar Pradesh"));
+        $(".table-responsive").shouldHave(text("Lucknow"));
 
     }
 }
