fed84be6205fd9d8190d8c4a0cde1082875e3f8b
s330mph <s330mph@gmail.com>
2025-06-22 19:50:47 +0300

    added SoftAssertionSearchTest

---
+++ src/test/java/tests/SoftAssertionSearchTest.java
@@ -0,0 +1,45 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SoftAssertionSearchTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com/";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void SoftAssertionSearch() {
+        open("/selenide/selenide/wiki");
+        $(".markdown-body").shouldHave(text("Soft assertions")).shouldBe(visible);
+        //open soft assertion
+        $(".markdown-body").$(byText("Soft assertions")).click();
+        //check examples
+        $(".markdown-body").shouldBe(text("Using JUnit5"));
+        $("#wiki-wrapper").shouldHave(text("""
+                @ExtendWith({SoftAssertsExtension.class})
+                class Tests {
+                  @Test
+                  void test() {
+                    Configuration.assertionMode = SOFT;
+                    open("page.html");
+                    $("#first").should(visible).click();
+                    $("#second").should(visible).click();
+                  }
+                }
+        """));
+    }
+
+}
+
