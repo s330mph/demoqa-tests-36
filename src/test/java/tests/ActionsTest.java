3ca112b7baab60fabee61de499a31fd567f1a136
s330mph <s330mph@gmail.com>
2025-06-30 10:14:29 +0300

    renamed ActionsTest renamed GithabSolutionEnterprisesTest added default conditions checking in ActionsTest

--- src/test/java/tests/ActionsTest.java
+++ src/test/java/tests/ActionsTest.java
@@ -1,8 +1,8 @@
 package tests;
 
-import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.DragAndDropOptions.to;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -10,14 +10,18 @@ public class ActionsTest {
     @Test
     void actionsWithOffsetTest(){
         open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
         actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(200, 0).release().perform();
-        $("#column-b").$("header").shouldHave(Condition.text("A"));
+        $("#column-b").$("header").shouldHave(text("A"));
     }
     @Test
     void dragndropTest(){
         open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
         $("#column-a").dragAndDrop(to("#column-b"));
-        $("#column-b").$("header").shouldHave(Condition.text("A"));
+        $("#column-b").$("header").shouldHave(text("A"));
 
     }
 }
